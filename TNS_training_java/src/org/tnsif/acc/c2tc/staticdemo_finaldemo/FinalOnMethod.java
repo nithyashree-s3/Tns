c2be8d2fac529d3dbf8693c3de43c0c87e4a63e4
nithyashree-s3 <nithyasri332005@gmail.com>
2026-09-22 15:37:55 +0530

    first commit

---
+++ TNS_training_java/src/org/tnsif/acc/c2tc/staticdemo_finaldemo/FinalOnMethod.java
@@ -0,0 +1,16 @@
+package org.tnsif.acc.c2tc.staticdemo_finaldemo;
+class Bank {
+    final void security_Rule() {
+        System.out.println("Follow KYC and OTP verification");
+    }
+}
+
+class SBI extends Bank {
+}
+
+public class FinalOnMethod {
+    public static void main(String[] args) {
+        SBI sbi = new SBI();
+        sbi.security_Rule();
+    }
+}
